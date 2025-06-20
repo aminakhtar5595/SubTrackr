56bdfabd0ff611958f3cb38b48c3eab48e1533e5
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-06-20 23:33:17 +0500

    Implemented splash UI

--- app/src/main/java/com/example/subtrackr/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/subtrackr/ui/screens/splash/SplashScreen.kt
@@ -1,8 +1,31 @@
 package com.example.subtrackr.ui.screens.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.subtrackr.R
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SplashScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(painter = painterResource(id = R.drawable.app_logo),
+            contentDescription = "App Logo")
 
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Image(painter = painterResource(id = R.drawable.app_icon),
+            contentDescription = "App Icon")
+    }
 }
