8dd30d7fdec8ddea2e4915b975d7d1f0e3afe514
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-06-21 09:49:51 +0500

    Implemented text input in search screen

--- app/src/main/java/com/example/subtrackr/MainActivity.kt
+++ app/src/main/java/com/example/subtrackr/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.subtrackr.ui.screens.home.HomeScreen
+import com.example.subtrackr.ui.screens.search.SearchScreen
 import com.example.subtrackr.ui.screens.splash.SplashScreen
 import com.example.subtrackr.ui.theme.SubTrackrTheme
 
@@ -18,7 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplashScreen()
+            SearchScreen()
             }
         }
     }
--- app/src/main/java/com/example/subtrackr/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/subtrackr/ui/screens/search/SearchScreen.kt
@@ -1,8 +1,69 @@
 package com.example.subtrackr.ui.screens.search
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.subtrackr.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen() {
+    var searchText by remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+    ) {
 
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(0xFFfffcd6))
+            .border(width = 2.dp, color = Color(0xFF689383), shape = RoundedCornerShape(10.dp))
+            .padding(horizontal =  12.dp),
+            verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(painter = painterResource(id = R.drawable.search_green_icon),
+            contentDescription = "App Icon", modifier = Modifier.size(30.dp))
+
+        TextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent,
+                textColor = Color.Gray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                placeholderColor = Color.LightGray
+            ),
+            textStyle = TextStyle(
+                fontSize = 20.sp, color = Color(0xFF689383), fontWeight = FontWeight.SemiBold),
+            placeholder = { Text("Search for records", style = TextStyle(fontSize = 20.sp), color = Color(0xFF9c9778), fontWeight = FontWeight.Medium) },
+        )
+    }
+    }
 }
