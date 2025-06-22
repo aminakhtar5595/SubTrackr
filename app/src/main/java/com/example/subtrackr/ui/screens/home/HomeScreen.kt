5dac4fd060e44cf1ed1a95d82d95cf4662548216
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-06-22 08:35:25 +0500

    Implemented confirm deletion dialog in home screen

--- app/src/main/java/com/example/subtrackr/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/subtrackr/ui/screens/home/HomeScreen.kt
@@ -1,9 +1,11 @@
 package com.example.subtrackr.ui.screens.home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,9 +13,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,10 +33,12 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.subtrackr.R
 
 @Composable
 fun HomeScreen() {
+    var showDialog by remember { mutableStateOf(false) }
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -124,6 +137,63 @@ fun HomeScreen() {
                     }
                 }
                 Text("$5,910.00", style = TextStyle(fontSize = 18.sp), color = Color(0xFFa94943), fontWeight = FontWeight.SemiBold)
+            }
+        }
+    }
+    if (showDialog) {
+        dialogView(
+            dismiss = { showDialog = false },
+        )
+    }
+}
+
+@Composable
+fun dialogView(dismiss: () -> Unit) {
+    Dialog(onDismissRequest = { dismiss() }) {
+        Column(
+            modifier = Modifier
+                .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+                .padding(horizontal = 32.dp, vertical = 24.dp)
+                .width(320.dp)
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Delete this record?",
+                style = TextStyle(fontSize = 20.sp),
+                color = Color(0xFF15433e),
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                "Are you sure?",
+                style = TextStyle(fontSize = 18.sp),
+                color = Color(0xFF15433e),
+                fontWeight = FontWeight.Medium
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                Button(
+                    onClick = { dismiss() },
+                    shape = RoundedCornerShape(5.dp),
+                    contentPadding = PaddingValues(horizontal = 40.dp, vertical = 12.dp),
+                    border = BorderStroke(2.dp, Color(0xFF15433e)),
+                    colors = ButtonDefaults.buttonColors(contentColor = Color(0xFF15433e), containerColor = Color.Transparent),
+                ) {
+                    Text("NO", style = TextStyle(fontSize = 20.sp), fontWeight = FontWeight.Medium)
+                }
+
+                Button(
+                    onClick = { dismiss() },
+                    shape = RoundedCornerShape(5.dp),
+                    contentPadding = PaddingValues(horizontal = 40.dp, vertical = 12.dp),
+                    border = BorderStroke(2.dp, Color(0xFF15433e)),
+                    colors = ButtonDefaults.buttonColors(contentColor = Color(0xFF15433e), containerColor = Color.Transparent),
+                ) {
+                    Text("YES", style = TextStyle(fontSize = 20.sp), fontWeight = FontWeight.Medium)
+                }
             }
         }
     }
