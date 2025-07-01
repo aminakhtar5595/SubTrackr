5ea51b7659cb56daaef6dae6a3a3ea0a7e3674d0
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-01 20:41:16 +0500

    Start working on expense screen

--- app/src/main/java/com/example/subtrackr/ui/screens/expense/ExpenseScreen.kt
+++ app/src/main/java/com/example/subtrackr/ui/screens/expense/ExpenseScreen.kt
@@ -1,8 +1,34 @@
 package com.example.subtrackr.ui.screens.expense
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.subtrackr.R
 
 @Composable
 fun ExpenseScreen() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFfffde8))
+            .padding(vertical = 20.dp),
+    ) {
+        Row {
 
+        }
+    }
 }
