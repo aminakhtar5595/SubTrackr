0a15607aabaedcfe79ffcdf2a34fda24437f3dcc
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-01 20:48:58 +0500

    Implemented top area of save and cancel in expense screen

--- app/src/main/java/com/example/subtrackr/ui/screens/expense/ExpenseScreen.kt
+++ app/src/main/java/com/example/subtrackr/ui/screens/expense/ExpenseScreen.kt
@@ -2,14 +2,18 @@ package com.example.subtrackr.ui.screens.expense
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,10 +29,29 @@ fun ExpenseScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color(0xFFfffde8))
-            .padding(vertical = 20.dp),
+            .padding(20.dp),
     ) {
-        Row {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row (
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Image(painter = painterResource(id = R.drawable.cancel_icon),
+                    contentDescription = "Cancel Icon", modifier = Modifier.size(22.dp))
+                Spacer(modifier = Modifier.padding(start = 8.dp))
+                Text("CANCEL", style = TextStyle(fontSize = 18.sp), color = Color(0xFF679384), fontWeight = FontWeight.Bold)
+            }
 
+            Row (
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Image(painter = painterResource(id = R.drawable.save_icon),
+                    contentDescription = "Cancel Icon", modifier = Modifier.size(22.dp))
+                Spacer(modifier = Modifier.padding(start = 8.dp))
+                Text("SAVE", style = TextStyle(fontSize = 18.sp), color = Color(0xFF679384), fontWeight = FontWeight.Bold)
+            }
         }
     }
 }
