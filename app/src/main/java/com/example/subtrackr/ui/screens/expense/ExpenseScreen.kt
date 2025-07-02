bf8a0d27d4eb68a37135738e69d57b155c09a4c0
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-02 10:08:39 +0500

    Implemented date and time area in expense screen

--- app/src/main/java/com/example/subtrackr/ui/screens/expense/ExpenseScreen.kt
+++ app/src/main/java/com/example/subtrackr/ui/screens/expense/ExpenseScreen.kt
@@ -6,21 +6,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -49,8 +42,7 @@ fun ExpenseScreen() {
     Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(0xFFfffde8))
-            .padding(vertical = 20.dp, horizontal = 10.dp),
+            .padding(top = 20.dp, start = 10.dp, end = 10.dp),
     ) {
         Row (
             modifier = Modifier.fillMaxWidth(),
@@ -89,22 +81,27 @@ fun ExpenseScreen() {
         }
 
         Row (
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
             modifier = Modifier.fillMaxWidth()
         ) {
             Column (
                 horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
             ) {
                 Text("Account", style = TextStyle(fontSize = 18.sp), color = Color(0xFF679384), fontWeight = FontWeight.Medium)
                 Spacer(modifier = Modifier.padding(bottom = 5.dp))
                 Row (
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp)
                         .border(
                             width = 2.dp,
                             color = Color(0xFF689383),
                             shape = RoundedCornerShape(6.dp)
                         )
-                        .padding(vertical = 15.dp, horizontal = 30.dp)
+                        .padding(horizontal = 12.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
                     Image(painter = painterResource(id = R.drawable.save_icon),
                         contentDescription = "Cancel Icon", modifier = Modifier.size(24.dp))
@@ -115,17 +112,22 @@ fun ExpenseScreen() {
 
             Column (
                 horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
             ) {
                 Text("Category", style = TextStyle(fontSize = 18.sp), color = Color(0xFF679384), fontWeight = FontWeight.Medium)
                 Spacer(modifier = Modifier.padding(bottom = 5.dp))
                 Row (
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp)
                         .border(
                             width = 2.dp,
                             color = Color(0xFF689383),
                             shape = RoundedCornerShape(6.dp)
                         )
-                        .padding(vertical = 15.dp, horizontal = 30.dp)
+                        .padding(horizontal = 12.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
                     Image(painter = painterResource(id = R.drawable.save_icon),
                         contentDescription = "Cancel Icon", modifier = Modifier.size(24.dp))
@@ -135,7 +137,7 @@ fun ExpenseScreen() {
             }
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(6.dp))
 
         TextField(
             value = notesText,
@@ -158,10 +160,10 @@ fun ExpenseScreen() {
                 )
                 .fillMaxWidth()
                 .background(color = Color(0xFFfffcd6))
-                .height(100.dp)
+                .height(105.dp)
         )
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(6.dp))
 
         Row(
             modifier = Modifier
@@ -380,6 +382,16 @@ fun ExpenseScreen() {
                     textAlign = TextAlign.Center
                 )
             }
+        }
+
+        Row (
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("June 20, 2025", style = TextStyle(fontSize = 20.sp), color = Color(0xFF679384), fontWeight = FontWeight.Medium, textAlign = TextAlign.Right)
+            Text("|", style = TextStyle(fontSize = 30.sp), color = Color(0xFF679384), fontWeight = FontWeight.ExtraBold)
+            Text("9:49 PM", style = TextStyle(fontSize = 20.sp), color = Color(0xFF679384), fontWeight = FontWeight.Medium)
         }
     }
 }
